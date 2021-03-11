1218c1c72bf1e56f8a24283c5960788576afad7a
aDahirel <axel.dahirel@gmail.com>
2021-03-11 12:14:52 +0100

    14. About Animation

--- src/animation.js
+++ src/animation.js
@@ -6,36 +6,44 @@ export const pageAnimation = {
   show: {
     opacity: 1,
     y: 0,
-    transtion: {
-      duration: 1,
+    transition: {
+      duration: 0.5,
+      when: "beforeChildren",
+      staggerChildren: 0.25,
     },
   },
   exit: {
     opacity: 0,
-    y: 300,
-    transtion: {
-      duration: 1,
+    transition: {
+      duration: 0.5,
     },
   },
 };
-/* 
-const titleAnim = {
+
+export const titleAnim = {
   hidden: { y: 200 },
   show: {
     y: 0,
     transition: { duration: 0.75, ease: "easeOut" },
   },
 };
-const container = {
-  hidden: { x: 100 },
+
+export const fade = {
+  hidden: { opacity: 0 },
   show: {
-    x: 0,
+    opacity: 1,
+    transition: { ease: "easeOut", duration: 0.75 },
+  },
+};
+
+export const photoAnim = {
+  hidden: { scale: 1.5, opacity: 0 },
+  show: {
+    scale: 1,
+    opacity: 1,
     transition: {
-      duration: 0.75,
       ease: "easeOut",
-      staggerChildren: 1,
-      when: "afterChildren",
+      duration: 0.75,
     },
   },
 };
- */
